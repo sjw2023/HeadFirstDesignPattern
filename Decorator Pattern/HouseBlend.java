87d61a8d5b278dfed9d14c5b9f04d5f12f4d9edb
JoowonSuh <sjw2023@naver.com>
2021-02-20 16:07:31 -0800

    Decorator Patter

---
+++ Decorator Pattern/HouseBlend.java
@@ -0,0 +1,8 @@
+public class HouseBlend extends Beverage{
+    public HouseBlend(){
+        description = "House Belnd";
+    }
+    public double cost(){
+        return .89;
+    }
+}
---
+++ Decorator Pattern/Soy.java
@@ -0,0 +1,13 @@
+public class Soy extends CondimentDecorator{
+    Beverage beverage; //for warpping
+    public Soy( Beverage beverage ){
+        this.beverage = beverage;
+    }
+
+    public String getDescription(){
+        return beverage.getDescription() + ", Soy";
+    }
+    public double cost(){
+        return .15 + beverage.cost();
+    }
+}
